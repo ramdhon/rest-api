6c9f1982638a75b219e0e45fb5c1c9113d987858
ramdhon <ramadhonimuhammad@gmail.com>
2019-04-26 10:18:43 +0700

    authentication updated

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,13 +1,24 @@
-const jwt = require('jsonwebtoken');
+const jwt = require('../helpers/jwt');
+const Model = require('../models');
 
 module.exports = (req, res, next) => {
   try {
-    let decoded = jwt.verify(req.headers.token, process.env.SECRET);
-    next();
+    let decoded = jwt.verify(req.headers.token);
+    Model
+      .User
+      .findOne({
+        where: {
+          username: decoded.username
+        }
+      })
+      .then(user => {
+        if (!user) {
+          res.status(400).json({ message: 'not recognized input data' });
+        } else {
+          next();
+        }
+      })
   } catch(err) {
-    res.status(401).json({
-      message: 'authentication failure',
-      err
-    });
+    res.status(400).json({ message: 'authentication failure', err });
   }
 }
